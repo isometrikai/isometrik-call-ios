fa306a560255ebd20a2628e486189ee9a5dd0555
Ajay Thakur <ajay@appscrip.co>
2024-06-27 19:10:20 +0530

    chore : update deployment target in package.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ISMSwiftCall",
-    platforms:[.iOS(.v14),
+    platforms:[.iOS(.v15),
     ], products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
